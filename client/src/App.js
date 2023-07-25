fbe17d8bcd11cc45f93e11f52fe4279f32fd4441
nhtt01 <nhtantai01@gmail.com>
2023-07-25 17:29:29 +0700

    commit
    get value username password in the input

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,16 @@ import LoginForm  from './components/auth/LoginForm'
 import RegisterForm  from './components/auth/RegisterForm'
 import Landing from './components/layouts/Landing';
 import Auth from './views/Auth';
+import AuthContextProvider from './contexts/authContext';
 function App() {
   return (
-  <>
+  <AuthContextProvider>
     <Routes>
     <Route extras path='/' element={<Landing/>}/>
         <Route path='/login' element={<Auth authRoute='login'/>}/>
         <Route path='/register' element={<Auth authRoute='register'/>}/>
     </Routes>
-  </>
+  </AuthContextProvider>
   );
 }
 
--- client/src/components/auth/LoginForm.js
+++ client/src/components/auth/LoginForm.js
@@ -1,8 +1,18 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../../contexts/authContext'
 function LoginForm() {
+	const [loginForm,setLoginForm] = useState({
+		username:'',
+		password:''
+	});
+	const {username ,password} = loginForm;
+	const onChangeLoginForm = (e)=>{
+		setLoginForm({...loginForm,[e.target.name]:e.target.value})
+	}
+	
   return (
 <>
 			<Form className='my-4' >
@@ -12,7 +22,8 @@ function LoginForm() {
 						placeholder='Username'
 						name='username'
 						required
-					
+						value={username}
+						onChange={onChangeLoginForm}
 					/>
 				</Form.Group>
 				<Form.Group>
@@ -21,7 +32,8 @@ function LoginForm() {
 						placeholder='Password'
 						name='password'
 						required
-				
+						value={password}
+						onChange={onChangeLoginForm}
 					/>
 				</Form.Group>
 				<Button variant='success' type='submit'>
